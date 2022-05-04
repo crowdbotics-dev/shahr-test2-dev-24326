1633151b6d02d100cb0d1ee8e3591c177021cf50
Crowdbotics <engineering@crowdbotics.com>
2022-05-04 21:31:46 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+class Post(models.Model):
+    'Generated Model'
+    caption = models.CharField(max_length=256,)
+    image = models.URLField()
+    description = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="post_user",)
